71e1c375ca9f207583e6220a35b155c19d42e2d1
Gabriel Ekodo <ekodogabriel@outlook.com>
2022-09-13 18:37:49 +0300

    updated config file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path"); 
+const path = require("path");
 const config = require("./config/database");
-const registerroutes= require("./routes/registerroutes");
+const registerroutes = require("./routes/registerroutes");
 const loginroutes = require("./routes/loginroutes");
 const purchasesroutes = require("./routes/purchaseroutes");
 const salesroutes = require("./routes/saleroutes");
@@ -11,40 +11,38 @@ const purchaselistroute = require("./routes/purchaselistroute");
 // instantiating server
 const app = express();
 const passport = require("passport");
-const Signup = require("./models/Signup")
+const Signup = require("./models/Signup");
 
 // setting up mongoose
-require("dotenv").config();
+require("dotenv").config({ path: "./config/config.env" });
 //connect mongoose
-mongoose.connect(config.database,{ useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 // Check connection
-db.once('open', function(){
-
-  console.log('Connected to MongoDB');
+db.once("open", function () {
+  console.log("Connected to MongoDB");
 });
 // Check for db errors
-db.on('error', function(err){
+db.on("error", function (err) {
   console.error(err);
 });
 
-
 //express sesssion
-const expressSession = require('express-session')({
-  secret: 'secret',
+const expressSession = require("express-session")({
+  secret: "secret",
   resave: false,
   saveUninitialized: false,
-  //cookie:{ maxAge:60*60*1000},  
+  //cookie:{ maxAge:60*60*1000},
 });
 
 // views settings or configurations
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 // middle ware
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(expressSession);
 
 app.use(passport.initialize());
@@ -53,31 +51,27 @@ passport.use(Signup.createStrategy());
 passport.serializeUser(Signup.serializeUser());
 passport.deserializeUser(Signup.deserializeUser());
 
-
 // const loginchecker = function(req, res, next){
 //     if(req.path != '/login' && req.path != '/' && !req.session.user){
 //       res.redirect ('/')
 //     }
 //     next()
 //   }
-  
+
 // app.use(loginchecker)
 // routes
 
-
-
-
-app.use('/', registerroutes)
-app.use('/', loginroutes)
-app.use('/', salesroutes)
-app.use('/', purchasesroutes)
-app.use('/', salelistroutes)
-app.use('/', purchaselistroute)
+app.use("/", registerroutes);
+app.use("/", loginroutes);
+app.use("/", salesroutes);
+app.use("/", purchasesroutes);
+app.use("/", salelistroutes);
+app.use("/", purchaselistroute);
 // handling non existing routes
-app.get('*', (req, res)=> {
-  res.status(404).send('OOPS! WRONG ADDRESS')
-})
-
+app.get("*", (req, res) => {
+  res.status(404).send("OOPS! WRONG ADDRESS");
+});
 
 // server
-app.listen(3002, ()=> console.log("Listening on Port 3002"));
+const port = process.env.PORT || 3002;
+app.listen(port, () => console.log(`server listen on port ${port}`));
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // database: "mongodb://localhost:27017/KGL_newdb",
-  database: "mongodb+srv://jose987654:1234567890@cluster0.l1t2vna.mongodb.net/KGL_database?retryWrites=true&w=majority",
+  database: process.env.DATABASE,
   // mongodb+srv://jose987654:<password>@cluster0.l1t2vna.mongodb.net/?retryWrites=true&w=majority
   secret: "mysecret",
 };
